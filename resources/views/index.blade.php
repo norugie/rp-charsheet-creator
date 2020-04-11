00f249a85453549b5ac5fa3fd5974af2396d371b
Rugie Ann Barrameda <norugie015@gmail.com>
2020-04-11 09:15:12 -0700

    add design to character list layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,15 @@
 
     {{-- Index - Characted Sheet List --}}
     @foreach ($characters as $character)
-        <h4>{{ $character->name }}</h4>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="/images/char_images/{{ $character->char_name }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">{{ $character->name }}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. A small river named Duden flows by their place.</p>
+                </div>
+            </div>
+        </div>
     @endforeach
 
 @endsection
