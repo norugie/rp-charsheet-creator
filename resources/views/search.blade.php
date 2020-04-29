09f6c59bf4bcc93623406c8110f2b2b873a83737
Rugie Ann Barrameda <norugie015@gmail.com>
2020-04-29 14:24:26 -0700

    character list per user

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,7 +9,11 @@
                 <img src="/images/char_images/{{ $character->char_filename }}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title text-center mb-0">{{ $character->char_name }}</h5>
-                    <p class="card-text mt-0 text-center">{{ $character->name }} | {{ $character->created_at->format('d M Y') }}</p>
+                    <p class="card-text mt-0 text-center">
+                        @if ( $character->username != 'anonuser' )
+                            <a href="/users/{{ $character->username }}">{{ $character->name }}</a>
+                        @else {{ $character->name }} @endif | {{ $character->created_at->format('d M Y') }}
+                    </p>
                     <p class="card-text">{{ $character->chardesc }}</p>
                     <center><a href="/character/{{ $character->slug }}" class="btn btn-dark">VIEW</a></center>
                 </div>
