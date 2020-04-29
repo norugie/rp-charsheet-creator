3d72df02671fb4f34d83698dd6374fa1799b44a9
Rugie Ann Barrameda <norugie015@gmail.com>
2020-04-29 11:12:47 -0700

    minor layout changes, added 404 and empty page

---
+++ resources/views/404.blade.php
@@ -0,0 +1,11 @@
+@extends ('layout.layout')
+
+@section ('content')
+
+    <div class="col-md-8 mx-auto text-center">
+        <h3>We are sorry - this page may have been deleted or does not exist</h3>
+        <h4 class="text-muted">Error 404 - Page not found</h4>
+        <p class="buttons"><a href="/" class="btn btn-dark">GO TO HOMEPAGE</a></p>
+    </div>
+
+@endsection
---
+++ resources/views/empty.blade.php
@@ -0,0 +1,11 @@
+@extends ('layout.layout')
+
+@section ('content')
+
+    <div class="col-md-8 mx-auto text-center">
+        <h3>We are sorry - no results were found for your query</h3>
+        <h4 class="text-muted">Error 204 - Empty response</h4>
+        <p class="buttons"><a href="/" class="btn btn-dark">GO TO HOMEPAGE</a></p>
+    </div>
+
+@endsection
