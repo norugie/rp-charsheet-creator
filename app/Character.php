4e54378752f2b7fd65a3fc312bb4b3a36d263e5e
Rugie Ann Barrameda <norugie015@gmail.com>
2020-04-30 10:10:43 -0700

    get author and images info in character page

--- app/Character.php
+++ app/Character.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Character extends Model
 {
-    //
+    public function author ()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function images ()
+    {
+        return $this->hasMany('App\Images', 'char_id');
+    }
 }
