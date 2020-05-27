1463e935a1cda900b1df2949cded7ba977088fef
Rugie Ann Barrameda <norugie015@gmail.com>
2020-05-26 22:29:46 -0700

    form setup

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,7 +3,32 @@
 @section ('content')
 
     {{-- Character - Create Character --}}
-
+    <div class="col-lg-12">
+        <form action="/create" method="POST">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="ion-ios-contact"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control clear-border" placeholder="First Name...">
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="ion-ios-contact"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control clear-border" placeholder="First Name...">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
     <textarea class="tinymce_editor">Next, use our Get Started docs to setup Tiny!</textarea>
 
 @endsection
