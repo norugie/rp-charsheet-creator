af75a8ecedf80dc2fe32009b2b6c6b7b3296a74c
Rugie Ann Barrameda <norugie015@gmail.com>
2020-08-28 13:27:35 -0700

    custom validation messages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <div class="col-lg-6 col-md-6 ml-auto mr-auto">
         <div class="card card-login py-4">
             <p class="description text-center">If you don't have an account yet, <a href="{{ route('register') }}">
-                sign up here!
+                register here!
             </a></p>
             <div class="card-body p-4">
                 @include ( 'auth.loginform' )
--- resources/views/auth/registerform.blade.php
+++ resources/views/auth/registerform.blade.php
@@ -1,75 +1,67 @@
-<form method="POST" action="{{ route('register') }}">
+<form class="needs-validation" method="POST" action="{{ route('register') }}" novalidate>
     @csrf
 
-    <div class="form-group row">
-        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-        <div class="col-md-6">
-            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">
+                <i class="ion-ios-person"></i>
+            </span>
         </div>
+        <input id="name" type="name" class="form-control clear-border @error('name') is-invalid @enderror" name="name" value="{{ old( 'name' ) }}" required autocomplete="name" placeholder="Name...">  
+        <div class="invalid-feedback">@error('name'){{ $message }}@else You cannot leave this section empty. @enderror</div>
     </div>
 
-    <div class="form-group row">
-        <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-        <div class="col-md-6">
-            <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-            @error('username')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">
+                <i class="ion-ios-at"></i>
+            </span>
         </div>
+        <input id="username" type="username" class="form-control clear-border @error('username') is-invalid @enderror" name="username" value="{{ old( 'username' ) }}" required autocomplete="username" placeholder="Username...">
+        <div class="invalid-feedback">@error('username'){{ $message }}@else You cannot leave this section empty. @enderror</div>
     </div>
 
-    <div class="form-group row">
-        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-        <div class="col-md-6">
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">
+                <i class="ion-ios-mail"></i>
+            </span>
         </div>
+        <input id="email" type="email" class="form-control clear-border @error('email') is-invalid @enderror" name="email" value="{{ old( 'email' ) }}" required autocomplete="email" placeholder="Email Address...">
+        <div class="invalid-feedback">@error('email'){{ $message }}@else You cannot leave this section empty. @enderror</div>
     </div>
 
-    <div class="form-group row">
-        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-        <div class="col-md-6">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">
+                <i class="ion-ios-lock"></i>
+            </span>
         </div>
+        <input id="password" type="password" class="form-control clear-border @error('password') is-invalid @enderror" name="password" required autocomplete="password" placeholder="Password...">
+        <div class="invalid-feedback">@error('password'){{ $message }}@else You cannot leave this section empty. @enderror</div>
     </div>
 
-    <div class="form-group row">
-        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-        <div class="col-md-6">
-            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">
+                <i class="ion-ios-checkmark-circle"></i>
+            </span>
         </div>
+        <input id="password-confirm" type="password" class="form-control clear-border @error('password') is-invalid @enderror" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password...">
+        <div class="invalid-feedback">You cannot leave this section empty.</div>
     </div>
-    <input type="text" name="char_id" value="{{ Request::is( 'character/*' ) ? $character->id : '' }}">
+
+    <input type="text" name="char_id" value="{{ Request::is( 'character/*' ) ? $character->id : '' }}" hidden>
     <div class="form-group row mb-0">
-        <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-dark">
-                {{ __('Register') }}
-            </button>
+        <div class="col-md-12 text-center">
+            <button type="submit" class="btn btn-dark">Register</button>
         </div>
     </div>
 </form>
+
+@section ( 'footer' )
+
+    {{-- Additional scripts for section: /register form --}}
+    <script src="/js/validation.js"></script>
+
+@endsection
